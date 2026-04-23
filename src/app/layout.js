c9f286f42dd4f5c39ae2137fb5e284fa1ee2cc6a
Marc <marcrnt@posteo.de>
2026-04-23 22:25:05 +0200

    add vercel speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col bg-background text-foreground antialiased">
         {children}
       </body>
+      <SpeedInsights/>
     </html>
   );
 }
