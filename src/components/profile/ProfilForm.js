3b41334d85e4ea522bf7159db684aad56626b97c
Marc <marcrnt@posteo.de>
2026-04-27 23:43:29 +0200

    few changes

--- src/components/profile/ProfilForm.js
+++ src/components/profile/ProfilForm.js
@@ -55,13 +55,13 @@ const SOCIAL_FIELDS = [
     name: "mastodonHandle",
     visKey: "mastodonVisibility",
     label: "Mastodon",
-    placeholder: "@nutzer@instanz.de",
+    placeholder: "nutzer@instanz.de",
   },
   {
     name: "pixelfedHandle",
     visKey: "pixelfedVisibility",
     label: "Pixelfed",
-    placeholder: "@nutzer@pixelfed.de",
+    placeholder: "nutzer@instanz.de",
   },
   {
     name: "blueskyHandle",
@@ -73,7 +73,7 @@ const SOCIAL_FIELDS = [
     name: "youtubeHandle",
     visKey: "youtubeVisibility",
     label: "YouTube",
-    placeholder: "@kanal",
+    placeholder: "nutzername",
   },
   {
     name: "twitchHandle",
