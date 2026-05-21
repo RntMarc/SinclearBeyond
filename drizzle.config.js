a4bd9c9ea959afa5e85eacd600210a2f8c9472a8
Marc <marcrnt@posteo.de>
2026-05-21 16:07:42 +0200

    set drizzle to utf8mb4_unicode_ci

--- drizzle.config.js
+++ drizzle.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
     password: process.env.DB_PASSWORD,
     port: Number(process.env.DB_PORT),
     ssl: false, // or your ssl config if required
-    uri: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?charset=utf8mb4_general_ci`
+    uri: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?charset=utf8mb4_unicode_ci`
   },
 });
