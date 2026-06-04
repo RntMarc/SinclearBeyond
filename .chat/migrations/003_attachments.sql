3ee9c526eff6228eccee8dde2e640165ffa3db37
Marc <marcrnt@posteo.de>
2026-06-05 00:46:34 +0200

    move chat code to submodule

--- .chat/migrations/003_attachments.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ChatMessages
-    DROP COLUMN attachment_url,
-    ADD COLUMN attachment_type VARCHAR(50) DEFAULT NULL AFTER body,
-    ADD COLUMN attachment_body LONGTEXT DEFAULT NULL AFTER attachment_type;
